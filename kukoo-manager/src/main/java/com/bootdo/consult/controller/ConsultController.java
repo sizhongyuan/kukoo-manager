79d56b48a5a6238c7a3d2f675f82df23f4e44227
honor-moment <tjpuwanggang@126.com>
2018-05-28 19:54:53 +0800

    咨询管理模块代码提交

--- kukoo-manager/src/main/java/com/bootdo/consult/controller/ConsultController.java
+++ kukoo-manager/src/main/java/com/bootdo/consult/controller/ConsultController.java
@@ -1,9 +1,17 @@
 package com.bootdo.consult.controller;
 
+import com.bootdo.common.annotation.Log;
+import com.bootdo.common.config.Constant;
 import com.bootdo.common.controller.BaseController;
+import com.bootdo.common.utils.MD5Utils;
+import com.bootdo.common.utils.PageUtils;
+import com.bootdo.common.utils.Query;
 import com.bootdo.common.utils.R;
 import com.bootdo.consult.model.Consult;
 import com.bootdo.consult.service.ConsultService;
+import com.bootdo.oa.domain.NotifyDO;
+import com.bootdo.system.domain.UserDO;
+import com.bootdo.system.service.UserService;
 import io.swagger.annotations.ApiOperation;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,32 +34,96 @@ public class ConsultController extends BaseController{
     private String prefix = "consult";
     @Autowired
     private ConsultService consultService;
+    @Autowired
+    UserService userService;
 
     @GetMapping()
-    @RequiresPermissions("consult:sysConsult:sysConsult")
+    @RequiresPermissions("consult:list")
     String consult() {
         return prefix + "/consult";
     }
 
+//    @ApiOperation(value="获取咨询列表", notes="")
+//    @ResponseBody
+//    @GetMapping("/list")
+//    @RequiresPermissions("consult:list")
+//    public List<Consult> list() {
+//        Map<String, Object> query = new HashMap<>(16);
+//        List<Consult> consultList = consultService.list(query);
+//        return consultList;
+//    }
+
     @ApiOperation(value="获取咨询列表", notes="")
     @ResponseBody
     @GetMapping("/list")
-    @RequiresPermissions("consult:sysConsult:sysConsult")
-    public List<Consult> list() {
+    @RequiresPermissions("consult:list")
+    public PageUtils list(@RequestParam Map<String, Object> params) {
+        // 查询列表数据
+        Query query = new Query(params);
+        query.put("parentId","0");
+        List<Consult> consultList = consultService.list(query);
+        int total = consultService.count(query);
+        PageUtils pageUtils = new PageUtils(consultList, total);
+        return pageUtils;
+    }
+
+    @ResponseBody
+    @GetMapping("/sublist/{parentId}")
+    @RequiresPermissions("consult:list")
+    public List<Consult> sublist(@PathVariable("parentId") Integer parentId) {
         Map<String, Object> query = new HashMap<>(16);
+        System.out.println("parentId=="+parentId);
+        query.put("parentId",parentId);
         List<Consult> consultList = consultService.list(query);
         return consultList;
     }
 
     @GetMapping("/add/{id}")
-    @RequiresPermissions("consult:sysConsult:add")
+    @RequiresPermissions("consult:add")
     String add(@PathVariable("id") Integer id, Model model) {
-        model.addAttribute("id", id);
+        Consult consult = consultService.get(id);
+        model.addAttribute("consult", consult);
         return  prefix + "/add";
     }
 
+    @GetMapping("/addConsultUser")
+    @RequiresPermissions("consult:add")
+    String addConsultUser(Model model) {
+        Consult consult = new Consult();
+        consult.setParentId(0);//parentId
+        consult.setAdviser(super.getUsername());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateStr = sdf.format(new Date());
+        consult.setCreateTime(dateStr);
+        model.addAttribute("consult", consult);
+        return  prefix + "/addConsultUser";
+    }
+
+    @RequiresPermissions("sys:user:add")
+    @Log("快速注册账号")
+    @PostMapping("/registerUser")
+    @ResponseBody
+    R registerUser(UserDO user) {
+        user.setPassword(MD5Utils.encrypt(user.getUsername(), user.getPassword()));
+        if (userService.save(user) > 0) {
+
+            Consult consult = new Consult();
+            consult.setAdviser(super.getUsername());
+            consult.setUserId(String.valueOf(user.getUserId()));
+            consult.setUserName(user.getUsername());
+            consult.setParentId(0);
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String dateStr = sdf.format(new Date());
+            consult.setCreateTime(dateStr);
+            consultService.save(consult);
+            return R.ok();
+        }
+        return R.error();
+    }
+
     @GetMapping("/edit/{id}")
-    @RequiresPermissions("consult:sysConsult:edit")
+    @RequiresPermissions("consult:edit")
     String edit(@PathVariable("id") Integer id, Model model) {
         Consult consult = consultService.get(id);
         model.addAttribute("consult", consult);
@@ -63,7 +135,7 @@ public class ConsultController extends BaseController{
      */
     @ResponseBody
     @PostMapping("/save")
-    @RequiresPermissions("consult:sysConsult:add")
+    @RequiresPermissions("consult:add")
     public R save(Consult consult) {
         consult.setAdviser(super.getUsername());
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -82,7 +154,7 @@ public class ConsultController extends BaseController{
      */
     @ResponseBody
     @RequestMapping("/update")
-    @RequiresPermissions("consult:sysConsult:edit")
+    @RequiresPermissions("consult:edit")
     public R update(Consult consult) {
         if (consultService.update(consult) > 0) {
             return R.ok();
@@ -95,7 +167,7 @@ public class ConsultController extends BaseController{
      */
     @PostMapping("/remove")
     @ResponseBody
-    @RequiresPermissions("consult:sysConsult:remove")
+    @RequiresPermissions("consult:remove")
     public R remove(Integer id) {
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("id", id);
@@ -110,7 +182,7 @@ public class ConsultController extends BaseController{
      */
     @PostMapping("/batchRemove")
     @ResponseBody
-    @RequiresPermissions("consult:sysConsult:batchRemove")
+    @RequiresPermissions("consult:batchRemove")
     public R remove(@RequestParam("ids[]") Integer[] ids) {
         consultService.batchRemove(ids);
         return R.ok();
--- kukoo-manager/src/main/resources/static/js/appjs/consult/consult.js
+++ kukoo-manager/src/main/resources/static/js/appjs/consult/consult.js
@@ -6,53 +6,72 @@ $(function() {
 
 function load() {
 	$('#exampleTable')
-		.bootstrapTreeTable(
+		.bootstrapTable(
 			{
 				id : 'id',
 				code : 'id',
-				type : "GET", // 请求数据的ajax类型
-				url : prefix + '/list', // 请求数据的ajax的url
+				parentCode : 'parentId',
+				method : 'get', // 服务器数据的请求方式 get or post
+				url : prefix + "/list", // 服务器数据的加载地址
+				//	showRefresh : true,
+				//	showToggle : true,
+				//	showColumns : true,
+				iconSize : 'outline',
+				toolbar : '#exampleToolbar',
 				ajaxParams : {}, // 请求数据的ajax的data属性
-				expandColumn : '1', // 在哪一列上面显示展开按钮
-				striped : true, // 是否各行渐变色
-				bordered : true, // 是否显示边框
+				expandColumn : '2', // 在哪一列上面显示展开按钮
 				expandAll : false, // 是否全部展开
+				detailView: true,//父子表
+				striped : true, // 设置为true会有隔行变色效果
+				dataType : "json", // 服务器返回的数据类型
 				pagination : true, // 设置为true会在底部显示分页条
 				// queryParamsType : "limit",
 				// //设置为limit则会发送符合RESTFull格式的参数
 				singleSelect : false, // 设置为true将禁止多选
+				// contentType : "application/x-www-form-urlencoded",
+				// //发送到服务器的数据编码类型
 				pageSize : 10, // 如果设置了分页，每页数据条数
 				pageNumber : 1, // 如果设置了分布，首页页码
-				search : true, // 是否显示搜索框
-				showColumns : true, // 是否显示内容下拉框（选择显示的列）
-				sidePagination : "client", // 设置在哪里进行分页，可选值为"client" 或者
-				// toolbar : '#exampleToolbar',
+				//search : true, // 是否显示搜索框
+				showColumns : false, // 是否显示内容下拉框（选择显示的列）
+				sidePagination : "server", // 设置在哪里进行分页，可选值为"client" 或者 "server"
+				queryParams : function(params) {
+					return {
+						//说明：传入后台的参数包括offset开始索引，limit步长，sort排序列，order：desc或者,以及所有列的键值对
+						limit : params.limit,
+						offset : params.offset,
+						userId:$('#searchName').val(),
+					    userName:$('#searchName').val(),
+						userTelephone:$('#searchName').val()
+					};
+				},
+				// //请求服务器数据时，你可以通过重写参数的方式添加一些额外的参数，例如 toolbar 中的参数 如果
+				// queryParamsType = 'limit' ,返回参数必须包含
+				// limit, offset, search, sort, order 否则, 需要包含:
+				// pageSize, pageNumber, searchText, sortName,
+				// sortOrder.
+				// 返回false将会终止请求
 				columns : [
-					{ // 列配置项
-						// 数据类型，详细参数配置参见文档http://bootstrap-table.wenzhixin.net.cn/zh-cn/documentation/
+					{
 						checkbox : true
-						// 列表中显示复选框
 					},
 					{
-						title : '编号',
+						visible :false,
 						field : 'id',
-						visible : false,
-						align : 'center',
-						valign : 'center',
-						width : '50px',
-						checkbox : true
+						title : '编号'
 					},
+
 					{
 						field : 'userId',
 						title : '用户ID',
-                        valign : 'center',
+						valign : 'center',
 						witth :20
 					},
 					{
 						field : 'userName',
 						title : '用户名',
-                        align : 'center',
-                        valign : 'center',
+						align : 'center',
+						valign : 'center',
 					},
 					{
 						field : 'userTelephone',
@@ -60,79 +79,96 @@ function load() {
 						align : 'center',
 						valign : 'center',
 					},
-					{
-						field : 'adviser',
-						title : '顾问',
-						align : 'center',
-						valign : 'center',
-					},
-					{
-						field : 'createTime',
-						title : '咨询时间',
-						align : 'center',
-						valign : 'center',
-					},
-					{
-						field : 'consultRecord',
-						title : '咨询记录',
-						align : 'center',
-						valign : 'center',
-					},
-					{
-						field : 'consultResult',
-						title : '咨询结果',
-						align : 'center',
-						valign : 'center',
-					},
 					//{
-					//	field : 'delFlag',
-					//	title : '状态',
+					//	field : 'adviser',
+					//	title : '顾问',
 					//	align : 'center',
-                     //   valign : 'center',
-					//	formatter : function(item, index) {
-					//		if (item.delFlag == '0') {
-					//			return '<span class="label label-danger">禁用</span>';
-					//		} else if (item.delFlag == '1') {
-					//			return '<span class="label label-primary">正常</span>';
-					//		}
-					//	}
+					//	valign : 'center',
+					//},
+					//{
+					//	field : 'createTime',
+					//	title : '咨询时间',
+					//	align : 'center',
+					//	valign : 'center',
+					//},
+					//{
+					//	field : 'consultRecord',
+					//	title : '咨询记录',
+					//	align : 'center',
+					//	valign : 'center',
+					//},
+					//{
+					//	field : 'consultResult',
+					//	title : '咨询结果',
+					//	align : 'center',
+					//	valign : 'center',
 					//},
 					{
+						visible : false,
+						field : 'delFlag',
+						title : '删除标记'
+					},
+					{
 						title : '操作',
-						field : 'id',
+						field : 'operation',
 						align : 'center',
-                        valign : 'center',
-						formatter : function(item, index) {
+						formatter : function(value, row, index) {
 							var e = '<a class="btn btn-primary btn-sm ' + s_edit_h + '" href="#" mce_href="#" title="编辑" onclick="edit(\''
-								+ item.id
+								+ row.id
 								+ '\')"><i class="fa fa-edit"></i></a> ';
 							var a = '<a class="btn btn-primary btn-sm ' + s_add_h + '" href="#" title="增加下級"  mce_href="#" onclick="add(\''
-								+ item.id
+								+ row.id
 								+ '\')"><i class="fa fa-plus"></i></a> ';
-							var d = '<a class="btn btn-warning btn-sm ' + s_remove_h + '" href="#" title="删除"  mce_href="#" onclick="removeone(\''
-								+ item.id
+							var d = '<a class="btn btn-warning btn-sm ' + s_remove_h + '" href="#" title="删除"  mce_href="#" onclick="remove(\''
+								+ row.id
 								+ '\')"><i class="fa fa-remove"></i></a> ';
-							var f = '<a class="btn btn-success btn-sm＂ href="#" title="备用"  mce_href="#" onclick="resetPwd(\''
-								+ item.id
+							var f = '<a class="btn btn-success btn-sm" href="#" title="备用"  mce_href="#" onclick="resetPwd(\''
+								+ row.id
 								+ '\')"><i class="fa fa-key"></i></a> ';
-							return e + a + d;
+							return a + d;
 						}
-					} ]
+					} ],
+				//注册加载子表的事件。注意下这里的三个参数！
+				onExpandRow: function (index, row, $detail) {
+					//alert(index+"--"+row.id+"--"+$detail);
+					InitSubTable(index, row, $detail);
+				}
 			});
 }
 function reLoad() {
-	load();
+	$('#exampleTable').bootstrapTable('refresh');
 }
-function add(pId) {
+function add(id) {
 	layer.open({
 		type : 2,
 		title : '增加',
 		maxmin : true,
 		shadeClose : false, // 点击遮罩关闭层
 		area : [ '800px', '520px' ],
-		content : prefix + '/add/' + pId
+		content : prefix + '/add/'+id // iframe的url
 	});
 }
+function addConsultUser() {
+	layer.open({
+		type : 2,
+		title : '增加',
+		maxmin : true,
+		shadeClose : false, // 点击遮罩关闭层
+		area : [ '800px', '520px' ],
+		content : prefix + '/addConsultUser' // iframe的url
+	});
+}
+function registerUser() {
+	layer.open({
+		type : 2,
+		title : '快速注册账号',
+		maxmin : true,
+		shadeClose : false, // 点击遮罩关闭层
+		area : [ '800px', '520px' ],
+		content : '/sys/user/add/' // iframe的url
+	});
+}
+
 function edit(id) {
 	layer.open({
 		type : 2,
@@ -143,7 +179,7 @@ function edit(id) {
 		content : prefix + '/edit/' + id // iframe的url
 	});
 }
-function removeone(id) {
+function remove(id) {
 	layer.confirm('确定要删除选中的记录？', {
 		btn : [ '确定', '取消' ]
 	}, function() {
@@ -200,3 +236,68 @@ function batchRemove() {
 	}, function() {});
 }
 
+//下面就是子表的结构 初始化
+var $child_table;
+window.InitSubTable = function (index, row, $detail) {
+	var parentId = row.id;
+	//alert(parentId);
+	var cur_table = $detail.html('<table></table>').find('table');
+	$(cur_table).bootstrapTable({
+		url : prefix + "/sublist/"+parentId, // 服务器数据的加载地址
+		method: 'get',
+		//queryParams: { parentId: parentId },
+		//queryParams : function(params) {
+		//	return {
+		//		//说明：传入后台的参数包括offset开始索引，limit步长，sort排序列，order：desc或者,以及所有列的键值对
+		//		//limit : params.limit,
+		//		//offset : params.offset,
+		//		parentId: parentId
+		//		// name:$('#searchName').val(),
+		//		// username:$('#searchName').val()
+		//	};
+		//},
+		//ajaxOptions: { parentId: parentId },
+		clickToSelect: true,
+		//detailView: true,//父子表
+		uniqueId: "id",
+		pageSize: 10,
+		pageList: [10, 25],
+		columns: [
+		//	{
+		//	checkbox: true
+		//},
+			{
+			field: 'adviser',
+			title: '顾问'
+		}, {
+			field: 'createTime',
+			title: '咨询时间'
+		}, {
+			field: 'consultRecord',
+			title: '咨询记录'
+		}, {
+			field: 'consultResult',
+			title: '咨询结果'
+		},{
+			title : '操作',
+			field : 'operation',
+			align : 'center',
+			formatter : function(value, row, index) {
+				var e = '<a class="btn btn-primary btn-sm ' + s_edit_h + '" href="#" mce_href="#" title="编辑" onclick="edit(\''
+					+ row.id
+					+ '\')"><i class="fa fa-edit"></i></a> ';
+				var a = '<a class="btn btn-primary btn-sm ' + s_add_h + '" href="#" title="增加下級"  mce_href="#" onclick="add(\''
+					+ row.id
+					+ '\')"><i class="fa fa-plus"></i></a> ';
+				var d = '<a class="btn btn-warning btn-sm ' + s_remove_h + '" href="#" title="删除"  mce_href="#" onclick="remove(\''
+					+ row.id
+					+ '\')"><i class="fa fa-remove"></i></a> ';
+				var f = '<a class="btn btn-success btn-sm" href="#" title="备用"  mce_href="#" onclick="resetPwd(\''
+					+ row.id
+					+ '\')"><i class="fa fa-key"></i></a> ';
+				return e + d;
+			}
+		} ]
+
+	});
+};
